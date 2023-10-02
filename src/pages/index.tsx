159eb08701899aa63ba2e2b9c814efeb386f6e3a
emre15900 <79264023+emre15900@users.noreply.github.com>
2023-10-02 16:58:11 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,23 @@
-import { Inter } from 'next/font/google'
-import { Grid, Card } from '@mui/material';
-
+import { Grid, Card } from "@mui/material";
 
 export default function Home() {
   return (
     <Grid>
       <Grid container spacing={6}>
         <Grid item xs={12} sm={6} md={3}>
-          <Card sx={{ background: "#F4F4F4", borderRadius: "30px", padding: "30px", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-
-          </Card>
+          <Card
+            sx={{
+              background: "#F4F4F4",
+              borderRadius: "30px",
+              padding: "30px",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          ></Card>
         </Grid>
       </Grid>
     </Grid>
-  )
+  );
 }
