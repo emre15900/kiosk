e2a4fd09ddf51dd3bd79322200872351251c1bec
emre15900 <79264023+emre15900@users.noreply.github.com>
2023-10-06 17:36:24 +0300

    update

--- src/pages/sendmoney.tsx
+++ src/pages/sendmoney.tsx
@@ -1,8 +1,44 @@
 import React from "react";
-import { Card, Grid, Typography, TextField, FormControl } from "@mui/material";
+import {
+  Card,
+  Grid,
+  Typography,
+  TextField,
+  FormControl,
+  InputLabel,
+  MenuItem,
+} from "@mui/material";
 import SelectMoney from "@/components/select-money";
 
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import PyButton from "@/components/py-button";
+
+const buttons = [
+  {
+    id: 1,
+    name: "Cash Pick Up",
+    img: "/images/cash-pick-up.png",
+  },
+  {
+    id: 2,
+    name: "Bank Transfer",
+    img: "/images/bank-transfer.png",
+  },
+];
+
 function SendMoney() {
+  const [active, setActive] = React.useState(1);
+
+  const handleClick = (id: number) => {
+    setActive(id);
+  };
+
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
+
   return (
     <Grid
       sx={{
@@ -94,6 +130,149 @@ function SendMoney() {
         >
           Choose a payment method
         </Typography>
+        <Grid
+          sx={{ display: "flex", alignItems: "center", gap: "20px", mt: 4 }}
+        >
+          {buttons.map((button) => {
+            return (
+              <Grid
+                onClick={() => handleClick(button.id)}
+                key={button.id}
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "10px",
+                  background: "#ffffff",
+                  borderRadius: "40px",
+                  padding: "15px 20px",
+                  border: active === button.id ? "1px solid #66FF84" : "",
+                  cursor: "pointer",
+                }}
+              >
+                <Grid sx={{ width: "80px" }}>
+                  <img src={button.img} alt="img" width="100%" />
+                </Grid>
+                <Typography variant="h6" sx={{ color: "#444094", mt: 1 }}>
+                  {button.name}
+                </Typography>
+              </Grid>
+            );
+          })}
+        </Grid>
+        <Grid
+          sx={{ mt: 4, display: "flex", alignItems: "center", gap: "10px" }}
+        >
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">
+              Choose payout option
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              label="Choose payout option"
+              onChange={handleChange}
+              sx={{
+                borderRadius: "30px",
+                borderColor: "#444094",
+                "& .MuiOutlinedInput-root": {
+                  borderRadius: "30px",
+                  color: "#444094",
+                },
+                "& .MuiOutlinedInput-notchedOutline": {
+                  borderColor: "#444094",
+                },
+              }}
+            >
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">
+              Choose payout branch
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              label="Choose payout branch"
+              onChange={handleChange}
+              sx={{
+                borderRadius: "30px",
+                borderColor: "#444094",
+                "& .MuiOutlinedInput-root": {
+                  borderRadius: "30px",
+                  color: "#444094",
+                },
+                "& .MuiOutlinedInput-notchedOutline": {
+                  borderColor: "#444094",
+                },
+              }}
+            >
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid
+          sx={{
+            background: "#444094",
+            borderRadius: "30px",
+            padding: "12px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            mt: 4,
+          }}
+        >
+          <Typography sx={{ color: "#ffffff" }}>0 USD = TRY</Typography>
+        </Grid>
+        <Grid sx={{ mt: 4 }}>
+          <Grid
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              gap: "10px",
+            }}
+          >
+            <Typography sx={{ color: "#000000", fontWeight: 200 }}>
+              Payable amount:
+            </Typography>
+            <Typography sx={{ color: "#000000", fontWeight: 200 }}>
+              500 USD
+            </Typography>
+          </Grid>
+          <Grid
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              gap: "10px",
+              mt: 3,
+            }}
+          >
+            <Typography sx={{ color: "#000000", fontWeight: 200 }}>
+              Transfer fee:
+            </Typography>
+            <Typography sx={{ color: "#000000", fontWeight: 200 }}>
+              5 USD
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            mt: 4,
+          }}
+        >
+          <PyButton title="Continues" padding="10px 5rem" />
+        </Grid>
       </Card>
     </Grid>
   );
